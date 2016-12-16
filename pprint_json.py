22ea9eab39641cad9c7570678bdaa9e9661684d6
niklsspb <prof051@yandex.ru>
2016-12-16 10:31:47 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
+# -*- coding: utf-8 -*-
 import json
+import os
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as json_file:
+        data_dict = json.load(json_file)
+    return data_dict
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
+file_path = input("Введите имя файла ")
+load_data = load_data(file_path)
+pretty_print_json(load_data)
 if __name__ == '__main__':
     pass
